f54cd1c947374ee0c4bbef809d04f0b4563ad10a
devmdtanvir <officialtanvir@hotmail.com>
2023-03-03 10:30:17 +0600

    I am frist using tailwind css but i am not open modal flowbite then i am using bootstrap thans working

--- index.js
+++ index.js
@@ -4,41 +4,39 @@ fetch("https://openapi.programming-hero.com/api/ai/tools")
   .then((data) => displayTechnology(data.data.tools));
 
 const displayTechnology = (data) => {
+  console.log(data);
   data.slice(0, 6).map((data) => {
-    console.log(data.features.map((data) => console.log(data)));
     const div = document.createElement("div");
+    div.classList.add("col");
     root.appendChild(div);
     div.innerHTML = `
-     
-<div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-<a href="#">
-    <img class="rounded-t-lg" src=${data.image} alt="" />
-</a>
-<div class="p-5">
-    <a href="#" class="text-white space-y-2">
-      
-        <p>Features</p>
-        <ul>
-          <li>${data.features[0]}</li>
-          <li>${data.features[1]}</li>
-          <li>${data.features[2]}</li>
-        </ul>
-    </a>
-    <h5 class="mb-2 text-4xl font-bold tracking-tight text-gray-900 dark:text-white">${data.name}</h5>
-   <div class="flex justify-between">
-   <a class="text-white">
-   <i class="fa-solid fa-calendar-days"></i>
-   ${data.published_in}
-   </a>
-    <button>
-    <i class="fa-solid fa-arrow-right text-white"></i>
-    </button>
-   </a>
-   </div>
-   
+    <div class="card" style="width: 18rem;">
+  <img src=${data.image} class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">${data.name}</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <div class="d-flex  justify-content-between">
+    <div>
+      <i class="fa-solid fa-calendar-days"></i>
+      <span>${data.published_in}</span>
+    </div>
+    <a href="#" class="btn btn-primary"  data-bs-toggle="modal"
+    data-bs-target="#staticBackdrop" onclick="loadTechnologyDetails(${data.id})">Go somewhere</a>
+    </div>
+  </div>
 </div>
-</div>
-
     `;
   });
+};
+
+const loadTechnologyDetails = (id) => {
+  fetch(`https://openapi.programming-hero.com/api/ai/tool/${"0" + id}`)
+    .then((res) => res.json())
+    .then((data) => displayTechnologyDetails(data.data));
+};
+
+const displayTechnologyDetails = (data) => {
+  console.log(data);
+
+  document.getElementById("modal-title").innerText = data.tool_name;
 };
